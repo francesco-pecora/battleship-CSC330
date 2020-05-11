d41c38b4e98c36610fdf553ffbf2d5e7d7ab1fe9
konstantinNovichenko <konstantin.novichenko@cix.csi.cuny.edu>
2020-05-11 05:43:08 -0400

    Working Demo Version (with simple random AI)
    - separated Board class from the runner
    - Board has logic to place ships randomly for computer
    - ComputerBoard class extends Board, overwrites On Click Handler, uses enemy Board as a parameter in constructor
    - ComputerBoard has simple AI (random attacks)
    - GUI separation approach was used based on the files provided by Sarthak
    - Game ends when one of the boards loses

---
+++ battleship/byKN/GameLevelGUI.java
@@ -0,0 +1,66 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+
+
+
+public class GameLevelGUI {
+	private BorderPane window;	
+	
+	
+	Board playerBoard = new Board(true);
+	ComputerBoard aiBoard = new ComputerBoard(false, playerBoard);
+	HBox aiHbox = aiBoard.start();    	
+	HBox playerHbox2 = playerBoard.start();
+	
+	double offsetX, offsetY;
+	 
+	Cruiser ship1 = new Cruiser(); 
+	ImageView base = new ImageView(new Image("file:src/assets/station.png"));
+	
+	public GameLevelGUI() {
+		window = new BorderPane();
+		BackgroundImage backImage = new BackgroundImage(new Image("file:src/assets/Background/stars_texture.png"),  BackgroundRepeat.REPEAT, 
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+		          BackgroundSize.DEFAULT);
+		window.setBackground(new Background(backImage));
+		window.setPrefSize(1200, 1000);
+		initGrid();
+	}
+	
+	private void initGrid() {
+		// TODO Auto-generated method stub
+		VBox finalBox = new VBox(20, aiHbox, playerHbox2);
+		AnchorPane GUISetup = new AnchorPane(finalBox);
+		
+		finalBox.setLayoutX(200);
+		finalBox.setLayoutY(20);
+		
+		
+		
+		
+        base.setLayoutX(30);
+        base.setLayoutY(630);
+        base.setScaleX(1.25);
+        base.setScaleY(1.25);        
+        
+        
+		window.getChildren().add(GUISetup);
+	}
+	
+	public Pane getRoot() {
+		return window; 
+	} 
+	
+	
+
+}
--- battleship/byKN/Ship.java
+++ battleship/byKN/Ship.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import javafx.scene.paint.Color;
 
 public abstract class Ship {
 	
